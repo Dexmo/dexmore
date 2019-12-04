526f3f32620e64532dcb699e2772c41a71037de6
Dexmo <moxmo73@gmail.com>
2019-12-04 21:18:34 +0100

    Simple visualisation created by matplotlib

---
+++ matemat.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+
+input_values = [1, 2, 3, 4, 5]
+squares = [1, 4, 9, 16, 25]
+plt.plot(input_values,squares, linewidth=5)
+
+#defining title and axis labels
+plt.title("Squares of numbers", fontsize=24)
+plt.xlabel("Values", fontsize=14)
+plt.ylabel("Squares of values", fontsize=14)
+
+#defining the label's dimentions
+plt.tick_params(axis='both', labelsize=14)
+
+plt.show()
